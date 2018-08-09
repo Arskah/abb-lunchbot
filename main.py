d5213468288f4afbcc6b485e45e88ff431c8a69e
Aarni Halinen <aarni.halinen@fi.abb.com>
2018-08-09 18:40:57 +0300

    Add Factory and exception handling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from bs4 import BeautifulSoup, SoupStrainer
 from datetime import datetime as dt
+from datetime import timedelta
 from slackclient import SlackClient
 
 slack_token = os.environ["SLACK_API_TOKEN"]
@@ -9,7 +10,6 @@ sc = SlackClient(slack_token)
 urls = {
     'Amica Tellus': "https://www.lounaat.info/lounas/amica-tellus/helsinki",
     # 'POR': "http://www.por.fi/Menu-Pitajanmaki",
-    # # 'Theron': "http://eatwork.fi/tilat/valimotie-27/",
     'Theron': "https://www.lounaat.info/lounas/theron-catering-pitjnmki/helsinki",
     # 'Factory': "http://www.ravintolafactory.com/lounasravintolat/ravintolat/helsinki-pitajanmaki/",
     'Blancco': "https://www.lounaat.info/lounas/blancco-pitajanmaki/helsinki",
@@ -34,31 +34,31 @@ def weekday_str():
 
 
 def parse(html_txt, weekday, restaurant):
-    amica_tags = SoupStrainer(id='menu')
-    tag = amica_tags        # Default to lounaat.info
-    if (restaurant == 'POR'):
-        por_tags = SoupStrainer(id='Printable')
-        tag = por_tags
-    # elif (restaurant == 'Theron'):
-    #     theron_tags = SoupStrainer(id='after_section_1')
-    #     tag = theron_tags
-    elif (restaurant == 'Factory'):
-        factory_tags = SoupStrainer(id='main')          # Fix
-        tag = factory_tags
-    # elif (restaurant == 'Blancco'):
-    #     blancco_tags = SoupStrainer(id='page-42')     # Fix
-    #     tag = blancco_tags
-    # elif (restaurant == 'Smarteat'):
-    #     smarteat_tags = SoupStrainer(id='textwidget') # Fix
-    #     tag = smarteat_tags
-    else:       # lounaat.info parse
-        soup = BeautifulSoup(html_txt, 'html.parser', parse_only=tag)
-        raw_html = soup.find(lambda tag: tag.name == "h3" and tag.text.lower().startswith(weekday)).parent.parent
-        lst = []
-        for elem in raw_html.find_all("p"):
-            lst.append(" ".join(elem.text.split()))
-        return '\n'.join(lst)
-    return ""       # Default failed parse
+    try:
+        amica_tags = SoupStrainer(id='menu')
+        tag = amica_tags        # Default to lounaat.info
+        if (restaurant == 'POR'):
+            por_tags = SoupStrainer(id='Printable')
+            tag = por_tags
+        elif (restaurant == 'Factory'):
+            # Load whole page since Factory has no good ids to parse into
+            soup = BeautifulSoup(html_txt, 'html.parser')
+            # Find parent of lounaslista text, which holds whole week in multiple <p>'s (*sigh :( )
+            s = soup.find(lambda tag: tag.name == "h2" and tag.text.lower().startswith("lounaslista")).parent.text
+            # Find today and tomorrow from the huge string which has whole week lunch and some bloat text
+            s = (s[s.lower().find(weekday) + len(weekday):s.lower().find(WEEKDAYS[(dt.today() + timedelta(days=1)).weekday()])])
+            # Get rid of date text that is following the weekday and return the text
+            s = (s[s.find("\n") + len("\n"):])
+            return s
+        else:       # lounaat.info parse
+            soup = BeautifulSoup(html_txt, 'html.parser', parse_only=tag)
+            raw_html = soup.find(lambda tag: tag.name == "h3" and tag.text.lower().startswith(weekday)).parent.parent
+            lst = []
+            for elem in raw_html.find_all("p"):
+                lst.append(" ".join(elem.text.split()))
+            return '\n'.join(lst)
+    except Exception:
+        return ""       # Default failed parse
 
 
 def main():
@@ -69,6 +69,7 @@ def main():
         print("{0}: {1}".format(restaurant, resp.status_code))
         text = parse(resp.text, wd, restaurant)
         # with open("test.html", 'a', encoding='utf-8') as f:
+        #     f.write(text)
         bolded_header = "*{0}:*\n".format(restaurant)
         sc.api_call(
             "chat.postMessage",
